3a3f9e287a602718f36aaca80df3f46a30c8795f
Elisa Jacobo <elisaj@mit.edu>
2022-11-30 22:30:50 -0500

    added imageUrl and fixed used ingredients

--- server/recipe/util.ts
+++ server/recipe/util.ts
@@ -11,13 +11,54 @@ type RecipeIngredient = {
 type RecipeResponse = {
   _id: number;
   name: string;
+  imageUrl: string;
   ingredients: RecipeIngredient[];
   instructions: string[];
   source: string;
   usedCount?: number;
-  usedIds?: Set<number>;
+  usedNames?: string[];
 };
 
+
+/**
+ * Transform a recipe response from the api into an object
+ * with all the information needed by the frontend with used 
+ * ingredient information
+ *
+ * @param {Object} recipe - a recipe response from the api
+ * @returns {RecipeResponse} - The recipe object
+ */
+const constructSuggestedRecipeResponse = (recipe: Dictionary<any>): RecipeResponse => {
+  // Find all used ingredients, if any
+  const usedIds = new Set<number>;
+  const usedNames:Array<string> = [];
+  for (const ingredient of recipe.usedIngredients) {
+    usedIds.add(ingredient.id);
+    usedNames.push(ingredient.name);
+  }
+  
+  // Create recipe ingredients
+  const ingredients = recipe.extendedIngredients.map((ingredient: Dictionary<any>) => {
+    return {
+      _id: ingredient.id,
+      name: ingredient.name !== ingredient.nameClean ? [ingredient.name, ingredient.nameClean] : [ingredient.name],
+      amount: ingredient.amount,
+      unit: ingredient.unit,
+      status: (usedIds.has(ingredient.id)) ? "used" : "missing",
+    };
+  });
+
+  return {
+    _id: recipe.id,
+    name: recipe.title,
+    imageUrl: recipe.image,
+    ingredients,
+    instructions: recipe.analyzedInstructions.length ? recipe.analyzedInstructions[0].steps.map((step: any) => { return step.step; }) : [],
+    source: recipe.sourceUrl,
+    usedCount: recipe.usedIngredientCount,
+    usedNames: usedNames
+  };
+};
 
 /**
  * Transform a recipe response from the api into an object
@@ -26,47 +67,28 @@ type RecipeResponse = {
  * @param {Object} recipe - a recipe response from the api
  * @returns {RecipeResponse} - The recipe object
  */
-const constructRecipeResponse = (recipe: Dictionary<any>): RecipeResponse => {
-  // Find all used ingredients, if any
-  const usedIds = new Set<number>;
-  if ("usedIngredients" in recipe) {
-    for (const ingredient of recipe.usedIngredients) {
-      usedIds.add(ingredient.id);
-    }
-  }
-  
+ const constructQueryRecipeResponse = (recipe: Dictionary<any>): RecipeResponse => {
   // Create recipe ingredients
   const ingredients = recipe.extendedIngredients.map((ingredient: Dictionary<any>) => {
-    const ingredientResponse: RecipeIngredient = {
+    return {
       _id: ingredient.id,
       name: ingredient.name !== ingredient.nameClean ? [ingredient.name, ingredient.nameClean] : [ingredient.name],
       amount: ingredient.amount,
       unit: ingredient.unit,
     };
-
-    if ("usedIngredients" in recipe) {
-      ingredientResponse.status = (ingredient.id in usedIds) ? "used" : "missing";
-    }
-
-    return ingredientResponse;
   });
 
-  const recipeResponse:RecipeResponse = {
+  return {
     _id: recipe.id,
     name: recipe.title,
+    imageUrl: recipe.image,
     ingredients,
     instructions: recipe.analyzedInstructions.length ? recipe.analyzedInstructions[0].steps.map((step: any) => { return step.step; }) : [],
     source: recipe.sourceUrl,
   };
-
-  if ("usedIngredients" in recipe) {
-    recipeResponse.usedCount = recipe.usedIngredientCount;
-    recipeResponse.usedIds = usedIds;
-  }
-
-  return recipeResponse;
 };
 
 export {
-  constructRecipeResponse
+  constructSuggestedRecipeResponse,
+  constructQueryRecipeResponse
 };
